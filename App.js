508c65ada7e60499cec3ee4bbd25d0c1a5ccfe9b
edu9988 <edu.jerm@gmail.com>
2025-04-11 20:25:59 -0300

    Move App.js to conform to snack.expo

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import styles from './assets/styles.js'
+import styles from './src/assets/styles.js'
 
-import Splash from './screens/Splash'
-import Signup from './screens/Signup'
-import Login from './screens/Login'
-import Gallery from './screens/Gallery'
-import Services from './screens/Services'
+import Splash from './src/screens/Splash'
+import Signup from './src/screens/Signup'
+import Login from './src/screens/Login'
+import Gallery from './src/screens/Gallery'
+import Services from './src/screens/Services'
 
 export default () => (
   <SafeAreaProvider style={styles.provider}>
